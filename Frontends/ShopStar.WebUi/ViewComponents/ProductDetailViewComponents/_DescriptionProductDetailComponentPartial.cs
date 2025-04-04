1cd1100e726f948bb47da6c6552f7c052c1bbb7a
shirinze <sherize1373@gmail.com>
2025-04-04 15:58:07 +0300

    start comment microservice

--- Frontends/ShopStar.WebUi/ViewComponents/ProductDetailViewComponents/_DescriptionProductDetailComponentPartial.cs
+++ Frontends/ShopStar.WebUi/ViewComponents/ProductDetailViewComponents/_DescriptionProductDetailComponentPartial.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ShopStar.DtosLayer.Dtos.CatalogDtos.ProductDetailDtos;
 
 namespace ShopStar.WebUi.ViewComponents.ProductDetailViewComponents
 {
+    
     public class _DescriptionProductDetailComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DescriptionProductDetailComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7113/api/ProductDetails/GetByProductIdProductDetail?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<UpdateProductDetailDto>(jsonData);
+                return View(value);
+            }
             return View();
         }
     }
---
+++ Services/Comment/ShopStar.Comment/Entities/UserComment.cs
@@ -0,0 +1,14 @@
+﻿namespace ShopStar.Comment.Entities
+{
+    public class UserComment
+    {
+        public int UserName { get; set; }
+        public string NameSureName { get; set; }
+        public string? ImageUrl { get; set; }
+        public string Email { get; set; }
+        public string CommentDetail { get; set; }
+        public int Rating { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
